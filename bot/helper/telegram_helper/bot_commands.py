0cba99a59e917f5d00d84976754c5df1c8159cd6
SpEcHlDe <89847967+SpEcHlDe@users.noreply.github.com>
2021-12-25 04:55:46 -0600

    Initial commit

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,7 +1,7 @@
 class _BotCommands:
     def __init__(self):
         self.StartCommand = 'start'
-        self.MirrorCommand = 'mirror'
+        self.MirrorCommand = 'mirror', 'mir'
         self.UnzipMirrorCommand = 'unzipmirror'
         self.ZipMirrorCommand = 'zipmirror'
         self.CancelMirror = 'cancel'
